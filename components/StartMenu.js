39a80eedb9553646966e74ab87c18fd454213a0a
Kamran Fekri <KFekri@carproof.com>
2018-05-16 16:12:58 -0400

    added async storage check on menu screen.

--- components/StartMenu.js
+++ components/StartMenu.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from "react";
-import { AppRegistry, StyleSheet, Dimensions, View, Text, Image, Alert, Modal, TouchableHighlight } from "react-native";
+import { AppRegistry, StyleSheet, Dimensions, View, Text, Image, Alert, Modal, TouchableHighlight, AsyncStorage } from "react-native";
 import { GameLoop } from "react-native-game-engine";
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -9,15 +9,61 @@ import Game from './Game';
 export default class StartMenu extends PureComponent {
   constructor(props) {
     super(props);
+
     this.state = {
+        buttons : null,
         play: false
     };
+
+    this.isPreviousGameAvailable()
+  }
+
+  async isPreviousGameAvailable(){
+    data = await AsyncStorage.getItem('@CPGame:game')
+    if(data != null){
+        console.log("YES")
+        //yes
+        this.setState({
+            buttons:
+            <View>
+                <TouchableHighlight
+                    style={styles.submit}
+                    onPress={() => this.setState({play: true})}
+                    underlayColor='#fff'>
+                    <Text style={styles.submitText}>CONTINUE GAME</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    style={styles.submit}
+                    onPress={() =>{
+                        AsyncStorage.clear() //clearing prev game data
+                        this.setState({play: true})
+                    }}
+                    underlayColor='#fff'>
+                    <Text style={styles.submitText}>NEW GAME</Text>
+                </TouchableHighlight>
+            </View>
+        })            
+    } else {
+        this.setState({
+            buttons:
+                <View>
+                    <TouchableHighlight
+                        style={styles.submit}
+                        onPress={() => this.setState({play: true})}
+                        underlayColor='#fff'>
+                        <Text style={styles.submitText}>PLAY</Text>
+                    </TouchableHighlight>
+                </View>   
+        })  
+    }
   }
 
 
 
   render() {
-    if(this.state.play){
+    if(this.state.buttons == null){
+        return( <View></View>)
+    } else if(this.state.play){
         return(
             <Modal
                 animationType="slide"
@@ -36,12 +82,7 @@ export default class StartMenu extends PureComponent {
                 <Image style={{margin: 10, height: 50, width: 207.5}}
                      source={{uri: 'https://www.carproof.com/public/images/CARPROOFlogo-primary_flat.png'}}
                 />
-                <TouchableHighlight
-                    style={styles.submit}
-                    onPress={() => this.setState({play: true})}
-                    underlayColor='#fff'>
-                    <Text style={styles.submitText}>PLAY</Text>
-                </TouchableHighlight>
+                {this.state.buttons}
                 <TouchableHighlight
                     style={styles.submit}
                     onPress={() => null}
